df3edc246f170778f4b2d24bf202a7447d0f4ade
Vishnu Rajendran <rajendran.vishnu97@gmail.com>
2023-09-17 08:36:46 -0400

    Stuff Init

--- Assets/A Joke/Scripts/LoadScene.cs
+++ Assets/A Joke/Scripts/LoadScene.cs
@@ -6,10 +6,29 @@ using UnityEngine;
 public class LoadScene : MonoBehaviour
 {
     [SerializeField] private int sceneId = 1;
-
+    [SerializeField] private GameObject skipCanvas;
     private IEnumerator Start()
     {
-        yield return new WaitForSeconds(20.5f);
+        bool canSkip = false;
+        if (PlayerPrefs.HasKey("CanSkip"))
+        {
+            skipCanvas.gameObject.SetActive(true);
+            canSkip = true;
+        }
+
+        float timeStep = 0;
+        while (timeStep <= 1)
+        {
+            timeStep += Time.deltaTime / 20.5f;
+            if (canSkip && Input.GetKey(KeyCode.Return))
+            {
+                break;
+            }
+            yield return new WaitForEndOfFrame();
+        }
+        yield return new WaitForEndOfFrame();
         UnityEngine.SceneManagement.SceneManager.LoadScene(sceneId);
+        PlayerPrefs.SetInt("CanSkip",0);
+        PlayerPrefs.Save();
     }
 }
